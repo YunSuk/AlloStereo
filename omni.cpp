204466403736c7f304b7a62ba02a2f0281b43ad1
YunSuk <yunsukchang@gmail.com>
2015-07-02 19:15:37 -0700

    stereo in progress

--- omni.cpp
+++ omni.cpp
@@ -21,9 +21,62 @@ struct MyApp : OmniApp {
   virtual ~MyApp() {}
 
   virtual void onDraw(Graphics& g) {
+
+
+    int face = mOmni.face();
+    int resolution = mOmni.resolution();
+
+
+
+    
+    mShader.end();
+    glLoadIdentity();
+    
+    double x,y,z,camerax,cameray,cameraz,viewingDirX,viewingDirY,viewingDirZ,upX,upY,upZ;
+    double ratio,radians,wd2,ndfl;
+    double left,right,top,bottom,near=Lens.nearClip,far=Lens.farClip;
+    
+    /*if (stereo)
+      near = Lens.focalLength / 5;
+    */
+    ratio  = 1;
+    wd2     = resolution/2;
+    ndfl    = near / Lens.focalLength;
+
+    x = 1;
+    y = 0;
+    z = 0;
+    camerax=0;
+    cameray=0;
+    cameraz=-1;
+    
+    x *= Lens.eyeSep / 2.0;
+    y *= Lens.eyeSep / 2.0;
+    z *= Lens.eyeSep / 2.0;
+
+    //left eye
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    left  = - ratio * wd2 - 0.5 * Lens.eyeSep * ndfl;
+    right =   ratio * wd2 - 0.5 * Lens.eyeSep * ndfl;
+    top    =   wd2;
+    bottom = - wd2;
+    glFrustum(left,right,bottom,top,near,far);
+    glMatrixMode(GL_MODELVIEW);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(camerax + x,cameray + y,cameraz + z,
+      camerax + x + viewingDirX,
+      cameray + y + viewingDirY,
+      cameraz + z + viewingDirZ,
+      upX,upY,upZ);
+
     light();
-    // say how much lighting you want
-    shader().uniform("lighting", 1.0);
+  // say how much lighting you want
+  //shader().uniform("lighting", 1.0);
+  //glUseProgramObjectARB(0);
+  //glUseProgram(NULL);
+
     g.draw(mesh);
   }
 
